2cb8de25f467efc32341f0e03fac1f11d03a6962
Imane <elkerzabi.imane@gmail.com>
2026-06-08 02:34:48 +0100

    fix: use PORT environment variable

--- backend/index.js
+++ backend/index.js
@@ -39,6 +39,7 @@ app.delete('/todos/:id', async (req, res) => {
   res.json({ message: 'Supprimé' })
 })
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000')
-})
+const PORT = process.env.PORT || 3000
+ app.listen(PORT, () => {
+   console.log(`Server is running on port ${PORT}`)
+ })
